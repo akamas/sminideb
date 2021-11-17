e468436510c940f4d06ef985a31568e31ae694b1
SAnker <akamas@outlook.de>
2021-11-17 02:18:14 +0100

    Create install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,10 @@
+apt install nano curl htop snapd make xfce4 dbus-x11 xrdp net-tools davfs2 sshfs nfs-common ifupdown2 qemu-guest-agent apparmor apparmor-utils apparmor-profiles apparmor-profiles-extra -y \
+&& hostnamectl set-hostname sminideb \
+
+&& cd /usr/local/src \
+wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz \
+tar xzf noip-duc-linux.tar.gz \
+cd noip-2.1.9-1 \
+make \
+make install 
+
