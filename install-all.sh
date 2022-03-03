79563977f552f4cfb99ea5915c8ed07718785561
SAnker <akamas@outlook.de>
2022-03-03 14:32:47 +0100

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,8 +1,7 @@
-apt update && apt full-upgrade -y && apt install -y snapd nano wget curl git htop davfs2 sshfs nfs-common qemu-guest-agent && systemctl start qemu-guest-agent && systemctl enable qemu-guest-agent \
+apt update && apt full-upgrade -y && apt install -y snapd nano wget curl git htop sshfs nfs-common qemu-guest-agent && systemctl start qemu-guest-agent && systemctl enable qemu-guest-agent \
 && apt install -y apparmor-utils apparmor-profiles apparmor-profiles-extra && service apparmor start \
 && apt install -y cockpit cockpit-ws cockpit-storaged cockpit-networkmanager cockpit-packagekit cockpit-machines \
 && hostnamectl set-hostname sminideb \
-&& curl -sSLf https://get.k0s.sh | sh -  \
 && cd /usr/local/src \
 && wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz \
 tar xzf noip-duc-linux.tar.gz \
