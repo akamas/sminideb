5aecac533310d4eed2926c46355c784fbbffa67b
SAnker <akamas@outlook.de>
2022-04-07 23:42:18 +0200

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,8 +1,6 @@
-apt install -y snapd nano wget curl git htop sshfs  \
-&& apt install -y apparmor-utils apparmor-profiles apparmor-profiles-extra \
-&& apt install -y cockpit* \
-&& hostnamectl set-hostname sminideb \
-&& cd /usr/local/src \
+hostnamectl set-hostname sminideb \
+&& mkdir /mnt/noip/
+&& cd /mnt/noip/ \
 && wget http://www.no-ip.com/client/linux/noip-duc-linux.tar.gz \
 tar xzf noip-duc-linux.tar.gz \
 cd noip-2.1.9-1 \
