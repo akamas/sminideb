688136eaf159c9f79a8b6850c7ae0c78d60b30f7
SAnker <akamas@outlook.de>
2022-04-07 23:22:35 +0200

    Update install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,4 @@
-apt update && apt full-upgrade -y && apt install -y snapd nano wget curl git htop sshfs nfs-common qemu-guest-agent && systemctl start qemu-guest-agent && systemctl enable qemu-guest-agent \
+apt update && apt full-upgrade -y && apt install -y snapd nano wget curl git htop sshfs qemu-guest-agent && systemctl start qemu-guest-agent && systemctl enable qemu-guest-agent \
 && apt install -y apparmor-utils apparmor-profiles apparmor-profiles-extra && service apparmor start \
 && apt install -y cockpit cockpit-ws cockpit-storaged cockpit-networkmanager cockpit-packagekit cockpit-machines \
 && hostnamectl set-hostname sminideb \
